fd51c53d596bdd0205134685482c1b755d15117b
Gunnar Stauning <gunnarstauning@u.boisestate.edu>
2021-03-10 13:12:40 -0700

    Steps 3A, 3B - DONE

--- project-3/random.js
+++ project-3/random.js
@@ -1,3 +1,36 @@
+//Step 3A.
+var htmlButton = document.getElementById('html-button');
+var htmlIsChanged = false;
+htmlButton.onclick = function(){
+    changeHTMLElement()
+};
+
+function changeHTMLElement(){
+    if(!htmlIsChanged){
+        htmlButton.innerHTML = "How dare you touch me?";
+        htmlIsChanged = true;
+    }
+    else{
+        htmlButton.innerHTML = "Change an HTML Element";
+        htmlIsChanged = false;
+    }
+}
+
+//Step 3B.
+var cssButton = document.getElementById('css-button');
+var cssIsChanged = false;
+cssButton.onclick = function(){
+    changeCSSElement()
+};
+
+function changeCSSElement(){
+    var buttons = document.getElementsByClassName('p3-buttons');
+    for(i=0; i < buttons.length; i++){
+        buttons[i].classList.toggle("new-p3-buttons");
+    }
+}
+
+//Step 3C.
 var messageText = document.getElementById('message-of-the-day');
 
 var messageSubmitButton = document.getElementById('message-submit-button');
